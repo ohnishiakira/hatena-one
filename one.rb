ac06c7dde2ac808ebfeaf2db0397a15fc468aeec
Akira Ohnishi <s06206ao@gmail.com>
2012-08-30 14:42:21 +0900

    がんばれぼくらのはてなOne

---
+++ one.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require "mechanize"
+
+class HatenaOne
+  def initialize(user, password)
+    @user     = user
+    @password = password
+    @agent    = Mechanize.new
+
+    login
+  end
+
+  def login
+    @agent.get("http://one.hatena.ne.jp/"){|page|
+      page.form_with(:action => "/login"){|form|
+        form.field_with(:id => "login-name"){|id| id.value = @user}
+        form.password = @password
+      }.submit
+    }
+  end
+
+  def post(comment)
+    @agent.get("http://one.hatena.ne.jp/o/timeline"){|one|
+      one.form_with(:action => "http://h2.hatena.ne.jp/post.text"){|form|
+        form.field_with(:id => "form-text-input"){|id| id.value = comment}
+      }.submit
+    }
+  end
+end
+
+one = HatenaOne.new(user, password)
+one.post(comment)
